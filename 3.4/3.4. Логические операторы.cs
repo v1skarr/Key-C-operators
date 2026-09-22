703a2e96c7bf7f85afa8b3229310f2b50dbd4045
v1skarr <159451585+v1skarr@users.noreply.github.com>
2026-09-22 21:55:50 +0300

    Add files via upload

---
+++ 3.4/3.4. Логические операторы.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._4.Логические_операторы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int x = 5 & 3;
+
+            //Console.WriteLine($"{x}");
+
+
+
+            //int x = 5 | 3;
+
+            //Console.WriteLine($"{x}");
+
+
+            //int x = 5 ^ 3;
+            //Console.WriteLine($"{x}");
+
+
+            //int x = ~0;
+            //Console.WriteLine($"{x}");
+
+
+
+            //int x = 1 << 4;
+            //Console.WriteLine($"{x}");
+
+
+            //int x = 40 >> 2;
+            //Console.WriteLine($"{x}");
+
+
+
+            //int n = 13;
+            //bool x = (n & 8) != 0;
+            //bool y = (n & (1 << 3)) != 0;
+            //if (x)
+            //{
+            //    Console.WriteLine("3-й бит установлен");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("3-й бит не установлен");
+            //}
+
+
+            //int n = 8;
+            //n |= (1 << 2);
+            //Console.WriteLine(n);
+
+
+            //int n = 20;
+            //n &= ~(1 << n);
+            //Console.WriteLine($"{n}");
+
+
+            //int x = (-16) >> 2;
+            //Console.WriteLine($"{x}");
+
+        }
+
+
+    }
+}
