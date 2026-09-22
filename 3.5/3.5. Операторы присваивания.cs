81df3a34af6dce5e00703e969644629f616bc0b2
v1skarr <159451585+v1skarr@users.noreply.github.com>
2026-09-22 21:56:12 +0300

    Add files via upload

---
+++ 3.5/3.5. Операторы присваивания.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _3._5.Операторы_присваивания
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 10;
+            //x += 5;
+            //Console.WriteLine($"{x}");
+
+
+            //int a = 10;
+            //a *= 2 + 3;
+            //Console.WriteLine($"{a}");
+
+
+            //int x = 12;
+            //x >>= 2;
+            //Console.WriteLine($"{x}");
+
+
+            //int? x = null;
+            //int y = 5;
+            //x = x ?? y;
+            //Console.WriteLine($"{x}");
+
+
+            //string str = null;
+            //str = str ?? "default";
+            //str = str ?? "custom";
+            //Console.WriteLine($"{str}");
+
+
+            //byte b = 1;
+            //b += 2; 
+            //Console.WriteLine($"{b}");
+
+
+            //int a = 5, b = 10, c = 0;
+            //c = a = b;
+            //Console.WriteLine($"{c}, {a}");
+
+
+            //int mask = 1;
+            //mask <<= 3;
+            //mask |= 2;
+            //Console.WriteLine($"{mask}");
+
+
+            //int x = 15;
+            //x %= 4;
+            //Console.WriteLine($"{x}");
+
+
+            //int x = 7;
+            //x ^= 7;
+            //Console.WriteLine($"{x}");
+        }
+    }
+}
