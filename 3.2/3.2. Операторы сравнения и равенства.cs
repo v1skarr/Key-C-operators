406cc09cb52a56f322074cebc6dbb2d638312482
v1skarr <159451585+v1skarr@users.noreply.github.com>
2026-09-22 21:55:09 +0300

    Add files via upload

---
+++ 3.2/3.2. Операторы сравнения и равенства.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2.Операторы_сравнения_и_равенства
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //bool x = 5 > 3;
+            //bool y = 5 >= 5;
+
+            //Console.WriteLine($"X = {x}");
+            //Console.WriteLine($"Y = {y}");
+
+
+
+            //bool x = "hello" == "hello";
+
+            //Console.WriteLine($"{x}");
+
+
+
+            //bool x = double.NaN == double.NaN;
+
+            //Console.WriteLine($"{x}");
+
+
+
+
+            //object a = new int[] { 1 };
+            //object b = new int[] { 1 };
+
+            //bool r = a == b;
+
+            //Console.WriteLine($"r = {r}");
+
+
+
+            //bool x = 10 != 10.0;
+
+            //Console.WriteLine($"x = {x}");
+
+
+
+            //bool x = null == null;
+
+            //Console.WriteLine($"x = {x}");
+
+
+
+            //bool x = (3 < 5) == (10 >= 20);
+
+            //Console.WriteLine($"x = {x}");
+
+
+
+            //bool res = 4 <= 4 && 5 > 2;
+
+            //Console.WriteLine($"res = {res}");
+
+
+
+            //char c = 'b';
+            //bool res = c > 'a';
+
+            //Console.WriteLine($"res = {res}");
+
+
+
+            //bool r = -0.0 > 0.0;
+
+            //Console.WriteLine($"r = {r}");
+
+
+        }
+    }
+}
