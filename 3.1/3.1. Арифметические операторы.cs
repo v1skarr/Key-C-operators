2d1f0d58735c9d658fe0ee505ab0a5396231cad2
v1skarr <159451585+v1skarr@users.noreply.github.com>
2026-09-22 21:53:52 +0300

    Add files via upload

---
+++ 3.1/3.1. Арифметические операторы.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Console.WriteLine("Вычисление результата");
+
+            //int x = 17 / 5;
+            //int y = 17 % 5;
+
+            //Console.WriteLine($"X = {x}");
+            //Console.WriteLine($"Y = {y}");
+
+
+
+            //Console.WriteLine("Вычислите значение res");
+
+            //int a = 5;
+            //int res = ++a * 2;
+
+            //Console.WriteLine($"res = {res}");
+
+
+
+            //Console.WriteLine("Значение res: ");
+
+            //int a = 5;
+            //int res = a++ * 2;
+
+            //Console.Write($"res = {res}");
+
+
+
+            //int C = 7 / 2;
+            //decimal P = 7.0m / 2;
+
+            //Console.WriteLine($"Решение целочисленного делеия: {C}");
+            //Console.WriteLine($"Решение целочисленного делеия: {P}");
+
+
+            //Console.WriteLine("Вычислите результат: ");
+
+            //int q = -15 % 4;
+
+            //Console.WriteLine($"Результат: {q}");
+
+
+
+            //Console.WriteLine("Вычислите: ");
+
+            //int x = 10;
+            //x = x++ + ++x;
+
+            //Console.WriteLine($"Решение {x}");
+
+
+
+            //int max = int.MaxValue;
+            //int res = checked(max + 1);
+
+            //Console.WriteLine($"Вывод{res}");
+
+
+            //int max = int.MaxValue;
+            //int res = unchecked(max + 1);
+
+            //Console.WriteLine($"Вывод {res}");
+
+
+
+            //Console.WriteLine("Вычислите: ");
+
+            //double x = 1.0 / 0.0;
+            //double y = 0.0 / 0.0;
+
+            //Console.WriteLine($"X = {x}");
+            //Console.WriteLine($"Y = {y}");
+
+
+
+            //int a = 8;
+            //int b = 3;
+            //int c = a - b * 2 + a / b;
+
+            //Console.WriteLine($"C = {c}");
+
+
+
+
+        }
+    }
+}
