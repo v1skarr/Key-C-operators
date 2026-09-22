cfea8f06b40d98f000672bc3fe477a0e07620de3
v1skarr <159451585+v1skarr@users.noreply.github.com>
2026-09-22 21:56:51 +0300

    Add files via upload

---
+++ 3.7/3.7. Операторы типов и приведения.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _3._7.Операторы_типов_и_приведения
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //object obj = "Hello";
+            //bool check = obj is string;
+            //Console.WriteLine($"{check}");
+
+
+            //object obj = 123;
+            //string s = obj as string;
+            //Console.WriteLine(s ?? "null");
+
+
+            //object obj = 123;
+            //string s = (string)obj;
+            //Console.WriteLine($"{s}");
+
+
+            //bool res = typeof(int) == typeof(Int32);
+            //Console.WriteLine($"{res}");
+
+
+            //int res = sizeof(long);
+            //Console.WriteLine($"{res}");
+
+
+            //object obj = null;
+            //bool res = obj is string;
+            //Console.WriteLine($"{res}");
+
+
+            //object x = null;
+            //bool b = x is null;
+            //Console.WriteLine($"{b}");
+
+
+            //int res = (int)3.99;
+            //Console.WriteLine($"{res}");
+
+
+            //object o = 42;
+            //if (o is int val && val > 40)
+            //{
+            //    Console.WriteLine($"val = {val}");
+            //}
+
+
+            //Console.WriteLine($"{default(int)}, {default(string) ?? "null"}");
+        }
+    }
+}
