7e50f4a7df8955dce51a38c4547be5234a804d1c
v1skarr <159451585+v1skarr@users.noreply.github.com>
2026-09-22 21:56:31 +0300

    Add files via upload

---
+++ 3.6/3.6. Тернарный и null-операторы.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._6.Тернарный_и_null_операторы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //int score = 75;
+            //string res = score >= 60 ? "Pass" : "Fail";
+            //Console.WriteLine($"{res}");
+
+
+            //int x = 5;
+            //int y = (x > 10) ? 100 : (x > 2) ? 50 : 0;
+            //Console.WriteLine($"{y}");
+
+
+            //double result = true ? 10 : 15.5;
+            //Console.WriteLine($"{result}");
+
+
+            //string s = null; 
+            //Console.WriteLine(s?.Length);
+
+
+            //string s = "Черенок";
+            //int? lenght = s?.Length;
+            //Console.WriteLine($"{lenght}");
+
+
+            //string name = null;
+            //string res = name ?? "Anonymous";
+            //Console.WriteLine($"{res}");
+
+
+            //string a = null, b = "User", c = "Admin";
+            //string res = a ?? b ?? c;
+            //Console.WriteLine($"{res}");
+
+
+            //int zero = 0;
+            //int result = false ? (10 / zero) : 42;
+            //Console.WriteLine($"{result}");
+
+
+            //bool condition = true;
+            //var x = condition ? 10 : "text";
+
+
+            //int? count = null;
+            //int res = count?.GetHashCode() ?? -1;
+            //Console.WriteLine($"{res}");
+
+
+        }
+    }
+}
